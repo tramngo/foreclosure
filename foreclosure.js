ffa2483b70b7ee101084d25c4bb2d497999a0f3f
Tram <tramngo2@gmail.com>
2016-09-09 11:17:29 -1000

    finished loan function

---
+++ foreclosure.js
@@ -0,0 +1,63 @@
+'use strict';
+var steve;
+var stevesLoan;
+var month = 0; 
+var monthsUntilEvicted; 
+
+function loan () {
+	var account={
+		borrowed: 55000,
+		balance: 286000,
+		monthlyPayment: 1700,
+		defaulted: 0,
+		defaultsToForeclose: 5,
+		foreclosed: 'false',
+	};
+
+function missPayment () {
+	account.defaulted++;  
+	}
+	if (account.defaulted >= account.defaultsToForeclose) {
+		return account.foreclosed = 'true';
+	}
+
+	return {
+		getBalance: function () {
+			return account.balance;	
+		}, 
+		receivePayment: function (amount) {
+			if (amount < account.monthlyPayment){
+				missPayment (); } 
+			account.balance -= amount; 
+		},
+		getMonthlyPayment: function () {
+			return account.monthlyPayment; 
+		},
+		isForeclosed: function () {
+			return account.foreclosed;
+		}
+	};
+}
+function borrower (loan) {
+	var account={
+		monthlyIncome: 1350,
+		funds: 2800,
+		loan: 'loan',
+
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
